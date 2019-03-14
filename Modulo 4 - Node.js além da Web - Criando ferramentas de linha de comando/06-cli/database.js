dbd61e7eb6ea20aab775ab314e27dad30f60eaac
Marcelo Wis <marcelowisc@gmail.com>
2019-03-13 23:44:10 -0300

    UPDATE

--- Modulo 4 - Node.js além da Web - Criando ferramentas de linha de comando/06-cli/database.js
+++ Modulo 4 - Node.js além da Web - Criando ferramentas de linha de comando/06-cli/database.js
@@ -1,23 +1,27 @@
-const { readFile, writeFile } = require('fs');
+const { writeFile, readFile } = require('fs');
 const { promisify } = require('util');
-
-const readFileAsync = promisify(readFile);
-const writeFileAsync = promisify(writeFile);
+const [writeFileAsync, readFileAsync] = [
+  promisify(writeFile),
+  promisify(readFile),
+];
 
 class Database {
   constructor() {
     this.FILENAME = 'heroes.json';
   }
-  async obterDadosArquivo() {
+
+  async obterArquivo() {
     const arquivo = await readFileAsync(this.FILENAME);
     return JSON.parse(arquivo.toString());
   }
+
   async escreverArquivo(dados) {
     await writeFileAsync(this.FILENAME, JSON.stringify(dados));
     return true;
   }
+
   async cadastrar(heroi) {
-    const dados = await this.obterDadosArquivo();
+    const dados = await this.obterArquivo();
     //workaround para simular um id
     const id = heroi.id <= 2 ? heroi.id : Date.now();
     const heroiComId = {
@@ -27,45 +31,46 @@ class Database {
 
     return await this.escreverArquivo([...dados, heroiComId]);
   }
+
   async listar(id) {
-    const dados = await this.obterDadosArquivo();
-
-    const dadosFilter = dados.filter(item => {
-      return id ? item.id === id : true;
-    });
-
-    return dadosFilter;
+    const dados = await this.obterArquivo();
+    // se nao passar o id, traz tudo
+    return dados.filter(item => (id ? item.id == id : true));
   }
-  async remover(id) {
-    const dados = await this.obterDadosArquivo();
-    const index = dados.findIndex(item => item.id !== parseInt(id));
 
-    if (index === -1) {
-      throw Error('heroi não existe');
+  async atualizar(id, atualizacoes) {
+    const dados = await this.obterArquivo();
+    const indice = dados.findIndex(item => item.id === parseInt(id));
+    if (indice === -1) {
+      throw Error('heroi não existe!');
     }
 
-    const atual = dados[index];
+    const atual = dados[indice];
+    dados.splice(indice, 1);
 
-    dados.splice(atual, 1);
-    console.log(dados);
+    //workaround para remover valores undefined do objeto
+    const objAtualizado = JSON.parse(JSON.stringify(atualizacoes));
+    const dadoAtualizado = Object.assign({}, atual, objAtualizado);
+
+    return await this.escreverArquivo([...dados, dadoAtualizado]);
+  }
+
+  async remover(id) {
+    if (!id) {
+      await this.escreverArquivo([]);
+      return true;
+    }
+
+    const dados = await this.obterArquivo();
+
+    const indice = dados.findIndex(item => item.id === parseInt(id));
+    if (indice === -1) {
+      throw Error('heroi não existe!');
+    }
+    const atual = dados[indice];
+    dados.splice(indice, 1);
     await this.escreverArquivo(dados);
     return true;
-  }
-  async atualizar(id, content) {
-    const dados = await this.obterDadosArquivo();
-    const index = dados.findIndex(item => item.id !== parseInt(id));
-    if (index === -1) {
-      throw Error('heroi não existe');
-    }
-    const atual = dados[index];
-
-    const objetoAtualizar = {
-      ...atual,
-      ...content,
-    };
-    dados.splice(index, 1);
-
-    return await this.escreverArquivo([...dados, objetoAtualizar]);
   }
 }
 
--- Modulo 4 - Node.js além da Web - Criando ferramentas de linha de comando/06-cli/test.js
+++ Modulo 4 - Node.js além da Web - Criando ferramentas de linha de comando/06-cli/test.js
@@ -1,41 +1,45 @@
-const { deepEqual } = require('assert');
-const database = require('./database');
-const DEFAULT_ITEM_CADASTRADO = { nome: 'Flash', poder: 'Speed', id: 1 };
+const { deepEqual, ok } = require('assert');
+const Database = require('./database');
+const DEFAULT_ITEM_CADASTRAR = { nome: 'Flash', poder: 'speed', id: 1 };
 const DEFAULT_ITEM_ATUALIZAR = {
   nome: 'Lanterna Verde',
   poder: 'Anel do poder',
   id: 2,
 };
 
-describe('Suite de manipulação de Herois', () => {
-  it('deve pesquisar um heroi, usando aquivos', async () => {
-    const expected = DEFAULT_ITEM_CADASTRADO;
-
-    const [resultado] = await database.listar(1);
-    deepEqual(resultado, expected);
+describe('Suite de manipulação de herois', () => {
+  before(async () => {
+    await Database.remover();
+    await Database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+    await Database.cadastrar(DEFAULT_ITEM_ATUALIZAR);
   });
 
   it('deve cadastrar um heroi', async () => {
-    const expected = DEFAULT_ITEM_CADASTRADO;
-    await database.cadastrar(DEFAULT_ITEM_CADASTRADO);
+    const expected = DEFAULT_ITEM_CADASTRAR;
+    await Database.cadastrar(DEFAULT_ITEM_CADASTRAR);
 
-    const [realResult] = await database.listar(expected.id);
+    const [realResult] = await Database.listar(expected.id);
     deepEqual(realResult, expected);
   });
-  it('deve remover um heroi por id', async () => {
-    await database.remover(1);
+
+  it('deve listar um heroi pelo id', async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR;
+    const result = await Database.listar(1);
+    deepEqual(result[0], expected);
   });
-  it.only('deve atualizar um heroi por id', async () => {
+
+  it('deve atualizar um heroi pelo id', async () => {
     const expected = {
       ...DEFAULT_ITEM_ATUALIZAR,
       nome: 'Batman',
-      poder: 'Dinheiro',
+      poder: 'ricão',
     };
-    const novoDado = { nome: 'Batman', poder: 'Dinheiro' };
+    await Database.atualizar(expected.id, {
+      nome: expected.nome,
+      poder: expected.poder,
+    });
 
-    await database.atualizar(DEFAULT_ITEM_ATUALIZAR.id, novoDado);
-    const [resultado] = await database.listar(DEFAULT_ITEM_ATUALIZAR.id);
-    console.log('resultado', resultado);
-    deepEqual(resultado, expected);
+    const [realResult] = await Database.listar(expected.id);
+    deepEqual(realResult, expected);
   });
 });
